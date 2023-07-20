946d25d53d3b433c373ac13f1a2f887218d552c3
haZuny <gkwns5791@naver.com>
2023-07-20 18:00:43 +0900

    testing data rename

--- Deep Learning/dataset/origin-data/Task_4_DCASE_2017_testing_set/data-select.py
+++ Deep Learning/dataset/origin-data/Task_4_DCASE_2017_testing_set/data-select.py
@@ -1,7 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 20 16:35:00 2023
+import csv, wave, os, array
+import numpy as np
 
-@author: hajun
-"""
 
+category_cnt = {}
+
+# route
+data_file_route = os.getcwd() + '\\testing-data\\'
+save_route = os.getcwd() + '\\rename\\'
+
+
+# csv file open
+f = open('testing-label.csv', 'r')
+reader = csv.reader(f)
+
+for line in reader:
+    # Read CSV line
+    label = line[0].split()
+    name = label[0]
+    start = float(label[1])
+    end = float(label[2])
+    category = '-'.join(label[3:])
+    
+    # Counting by Category
+    try:
+        category_cnt[category] += 1
+    except:
+        category_cnt[category] = 0
+    
+    # open wav file
+    w = w = wave.open(data_file_route + 'Y' + name, 'r')
+    frameRate = w.getframerate()
+    buf = w.readframes(w.getnframes())
+    amp = np.frombuffer(buf, dtype='int16')
+    
+    # data cutting
+    cut_amp = amp[int(start*frameRate) : int(end*frameRate)]
+    
+    # save
+    saveFileName = save_route + category + '-' + str(category_cnt[category]) + '.wav'
+    save_wave = wave.Wave_write(saveFileName)
+    save_wave.setparams(w.getparams())
+    save_wave.writeframes(array.array('h', cut_amp).tobytes())
+    save_wave.close()
+    
+f.close()
+
+# write summary
+f = open('summary.txt', 'w')
+f.write('categoty: count-per-category\n\n')
+for category in sorted(category_cnt.keys()):
+    f.write(f'{category}: {category_cnt[category]} \n')
+f.close
