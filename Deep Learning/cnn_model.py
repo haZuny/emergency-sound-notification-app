0002a0af27ec2bd558dc8a794f18e3f4185c6b4e
haZuny <gkwns5791@naver.com>
2023-08-01 05:18:31 +0900

    딥러닝 정확도 0.92

--- Deep Learning/cnn_model.py
+++ Deep Learning/cnn_model.py
@@ -2,9 +2,12 @@ import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
-import sound_processing
+import matplotlib.pyplot as plt
 
-#%% 데이터 로드
+import librosa
+
+
+# 데이터 로드
 arrSize = 1000
 X = []
 Y = []
@@ -22,9 +25,9 @@ for i in range(1, other_data_size+1):
     
 random.shuffle(data_idx)
 
-carHorn_path = './dataset/training-data/car_horn/'
-other_path = './dataset/training-data/other/'
-shuffle_path = './dataset/training-data/shuffle/'
+carHorn_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/car_horn/'
+other_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/other/'
+shuffle_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/shuffle/'
 
 
 for i in data_idx:
@@ -33,17 +36,20 @@ for i in data_idx:
     if i > 0:
         Y.append(1)
         # wav 파일 읽어서 stft
-        f, t, Zxx = sound_processing.wav_to_stft(shuffle_path+str(i-1)+'.wav')
+        #f, t, Zxx = sound_processing.wav_to_stft(shuffle_path+str(i-1)+'.wav')
+        audio, sample_rate = librosa.load(shuffle_path+str(i-1)+'.wav')
+        mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=64)
     # 그 외
     elif i < 0:
         Y.append(0)
         # wav 파일 읽어서 stft
-        f, t, Zxx = sound_processing.wav_to_stft(shuffle_path+str(carHorn_data_size - i -2)+'.wav')
-    Zxx = np.resize(Zxx, (129,1380, 1))
-    X.append(Zxx)
+        #f, t, Zxx = sound_processing.wav_to_stft(shuffle_path+str(carHorn_data_size - i -2)+'.wav')
+        audio, sample_rate = librosa.load(shuffle_path+str(carHorn_data_size - i -2)+'.wav')
+        mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=64)
+    mfcc = np.resize(mfcc, (64,256,1))
+    X.append(mfcc)
         
     
-
 X = np.array(X)
 Y = np.array(Y)
 
@@ -54,20 +60,21 @@ print(X.shape, Y.shape)
 #%% 모델 정의
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(129, 1380, 1)))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 256, 1)))
 model.add(layers.MaxPool2D(2,2))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
 model.add(layers.MaxPool2D(2,2))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Dropout(0.2,))
+model.add(layers.MaxPool2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Dropout(0.3,))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 # 컴파일
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
 
 # 학습
 X_train = X[:500]
@@ -75,15 +82,15 @@ X_val = X[500:]
 Y_train = Y[:500]
 Y_val= Y[500:]
 
-trained = model.fit(X_train, Y_train, epochs=8, validation_data=(X_val, Y_val))
+trained = model.fit(X_train, Y_train, epochs=50, validation_data=(X_val, Y_val))
 
 
-#%% 모델 저장
+# 모델 저장
 
 model.save('car_horn.h5')
 
 
-#%% 시각화
+# 시각화
 
 import matplotlib.pyplot as plt
 
@@ -105,6 +112,6 @@ plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
-model.summary()
+#model.summary()
 
 
