616407f0d5b32f8b15df124d34ae5ab0ee7095ff
haZuny <gkwns5791@naver.com>
2023-08-18 17:21:09 +0900

    딥러닝 모델 다시 만듦

--- Deep Learning/cnn_model.py
+++ Deep Learning/cnn_model.py
@@ -12,8 +12,8 @@ X = []
 Y = []
 
 # 데이터 로드
-carHorn_data_size = 342
-other_data_size = 403
+carHorn_data_size = 273
+other_data_size = 322
 
 # positive와 negative 순서 정의
 data_idx = []
@@ -24,9 +24,9 @@ for i in range(1, other_data_size+1):
     
 random.shuffle(data_idx)
 
-carHorn_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/car_horn/'
-other_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/other/'
-shuffle_path = 'D:/hajun/논문/emergency-sound-notification-app/Deep Learning/dataset/training-data/shuffle/'
+carHorn_path = 'D:/hajun/final/emergency-sound-notification-app/Deep Learning/dataset/training-data/car_horn/'
+other_path = 'D:/hajun/final/emergency-sound-notification-app/Deep Learning/dataset/training-data/other/'
+shuffle_path = 'D:/hajun/final/emergency-sound-notification-app/Deep Learning/dataset/training-data/shuffle/'
 
 
 for i in data_idx:
@@ -45,7 +45,7 @@ for i in data_idx:
         #f, t, Zxx = sound_processing.wav_to_stft(shuffle_path+str(carHorn_data_size - i -2)+'.wav')
         audio, sample_rate = librosa.load(shuffle_path+str(carHorn_data_size - i -2)+'.wav')
         mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=64)
-    mfcc = np.resize(mfcc, (64,256,1))
+    mfcc = np.resize(mfcc, (64,173,1))
     X.append(mfcc)
         
     
@@ -59,16 +59,17 @@ print(X.shape, Y.shape)
 #%% 모델 정의
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 256, 1)))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 173, 1)))
 model.add(layers.MaxPool2D(2,2))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
 model.add(layers.MaxPool2D(2,2))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
 model.add(layers.MaxPool2D(2,2))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
 model.add(layers.Dropout(0.3,))
 
 model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -81,7 +82,7 @@ X_val = X[500:]
 Y_train = Y[:500]
 Y_val= Y[500:]
 
-trained = model.fit(X_train, Y_train, epochs=50, validation_data=(X_val, Y_val))
+trained = model.fit(X_train, Y_train, epochs=15, validation_data=(X_val, Y_val))
 
 
 # 모델 저장
