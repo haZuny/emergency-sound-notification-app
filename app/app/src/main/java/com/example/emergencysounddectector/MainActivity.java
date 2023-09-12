5d00e88da8ba4d178408892fbf668ffbaf276959
haZuny <gkwns5791@naver.com>
2023-09-12 18:38:29 +0900

    화면 이동 설정

--- app/app/src/main/java/com/example/emergencysounddectector/MainActivity.java
+++ app/app/src/main/java/com/example/emergencysounddectector/MainActivity.java
@@ -2,14 +2,45 @@ package com.example.emergencysounddectector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Components
+    ImageButton btn_menuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 컴포넌트 초기화
+        btn_menuBtn = findViewById(R.id.main_button_menu);
+
+        // 메뉴 버튼 동작
+        btn_menuBtn.setOnClickListener(v->{
+            final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),v);
+            getMenuInflater().inflate(R.menu.menu_main,popupMenu.getMenu());
+            // 메뉴 클릭 동작
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    if (menuItem.getItemId() == R.id.menu_main_item1){
+                        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                        startActivity(intent);
+                    }else if (menuItem.getItemId() == R.id.menu_main_item2){
+                        Toast.makeText(MainActivity.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+            popupMenu.show();
+        });
     }
 }
