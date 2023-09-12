b23364805e9fcdcae53beb8f61a381464b16fd23
haZuny <gkwns5791@naver.com>
2023-09-12 15:41:21 +0900

    상태바 어둡게

---
+++ app/app/src/main/java/com/example/emergencysounddectector/HistoryIdxActivity.java
@@ -0,0 +1,14 @@
+package com.example.emergencysounddectector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryIdxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_idx);
+    }
+}
